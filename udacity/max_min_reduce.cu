2feb0eab950a5f8d2a4d1374bcc8123c0c1097bf
Valentin LEHUGER <vlehuger@e3r6p1.42.fr>
2015-05-10 09:42:20 +0200

    workin max reduce

--- udacity/max_min_reduce.cu
+++ udacity/max_min_reduce.cu
@@ -2,9 +2,8 @@
 #include <stdio.h>
 #include <cuda.h>
 
-#define NB 101
-#define BLOCK_SIZE 512
-#define GRID_SIZE NB / BLOCK_SIZE + 1
+#define NB 10
+#define BLOCK_SIZE 1024
 
 
 #define checkCudaErrors(val) check( (val), #val, __FILE__, __LINE__)
@@ -18,6 +17,18 @@ void check(T err, const char* const func, const char* const file, const int line
   }
 }
 
+#define cudaCheckErrors(msg) \
+    do { \
+        cudaError_t __err = cudaGetLastError(); \
+        if (__err != cudaSuccess) { \
+            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
+                msg, cudaGetErrorString(__err), \
+                __FILE__, __LINE__); \
+            fprintf(stderr, "*** FAILED - ABORTING\n"); \
+            exit(1); \
+        } \
+    } while (0)
+
 __global__ void max(float *d_in, float *d_out)
 {
 	int ft_id = threadIdx.x + blockDim.x * blockIdx.x;
@@ -28,7 +39,7 @@ __global__ void max(float *d_in, float *d_out)
 	{
 		if (ft_id + s < NB && tid < s)
 		{
-			printf("Compare d_in[%d] = %f with d_in[%d] = %f\n", (int)ft_id, d_in[ft_id], (int)(ft_id + s), d_in[ft_id + s]);
+			// printf("Compare d_in[%d] = %f with d_in[%d] = %f\n", (int)ft_id, d_in[ft_id], (int)(ft_id + s), d_in[ft_id + s]);
 			d_in[ft_id] = (d_in[ft_id] > d_in[ft_id + s]) ? d_in[ft_id] : d_in[ft_id + s];
 
 			if (size % 2 == 1 && ft_id + s + s == size - 1)
@@ -40,8 +51,10 @@ __global__ void max(float *d_in, float *d_out)
 	if (tid == 0)
 	{
 		d_out[blockIdx.x] = d_in[ft_id];
-		printf("Here d_out[%d] = %f\n", blockIdx.x, d_out[blockIdx.x]);
 	}
+	// __syncthreads();
+	// for (int i = 0; i < GRID_SIZE; i++)
+	// 	printf("d_out[%d] = %f\n", i, d_out[i]);
 }
 
 float			*init_values(size_t size)
@@ -60,42 +73,52 @@ float			*init_values(size_t size)
 
 int				main(void)
 {
+	size_t 		grid_size = NB / BLOCK_SIZE + 1;
+
 	float		*h_values;
 
 	float		*d_values;
 	float		**d_values_ = &d_values;
 
-	float		*h_max = (float *)malloc(sizeof(float) * GRID_SIZE);
+	float		*h_max = (float *)malloc(sizeof(float) * 1);
 	float		*d_max;
 	float		**d_max_ = &d_max;
 
 	h_values = init_values(NB);
 
-	// printf ("Initial values :\n");
-	// for (int i = 0; i < NB; i++)
-	// 	printf("%f\n", h_values[i]);
-	// printf("\n");
+	printf ("Initial values :\n");
+	for (int i = 0; i < NB; i++)
+		printf("%f\n", h_values[i]);
+	printf("\n");
 	
 
 	// malloc values and max
 	checkCudaErrors(cudaMalloc(d_values_, sizeof(float) * NB));
-	checkCudaErrors(cudaMalloc(d_max_, sizeof(float) * GRID_SIZE));
+	checkCudaErrors(cudaMalloc(d_max_, sizeof(float) * 1));
 
 	// memcopy values
 	checkCudaErrors(cudaMemcpy(d_values, h_values, sizeof(float) * NB, cudaMemcpyHostToDevice));
 
+	// // memset max
+	// checkCudaErrors(cudaMemset(d_max, 0, sizeof(float) * GRID_SIZE));
+
 	// kernel
-	max<<<GRID_SIZE, BLOCK_SIZE>>>(d_values, d_max);
+	max<<<grid_size, BLOCK_SIZE>>>(d_values, d_max);
+	cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+	
+	// printf("GRID SIZE %d\n", GRID_SIZE);
 
 	// memcpy max result
-	checkCudaErrors(cudaMemcpy(h_max, d_max, sizeof(float) * GRID_SIZE, cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaMemcpy(h_max, d_max, sizeof(float) * 1, cudaMemcpyDeviceToHost));
+
+
+
+	for (int i = 0; i < grid_size; i++)
+		printf("h_max[%d] = %f\n", i, h_max[i]);
 
 	// free the two
 	cudaFree(d_max_);
 	cudaFree(d_values_);
-
-	for (int i = 0; i < GRID_SIZE; i++)
-		printf("h_max[%d] = %f\n", i, h_max[i]);
 
 	return(0);
 }
